f4d7c4dc3d7743104bec1551d522985e09db5d24
rhuanhuan <1210098674@qq.com>
2018-02-28 22:36:28 +0800

    [Huan] Display my friend page

--- src/components/MyFriends/MyFriends.js
+++ src/components/MyFriends/MyFriends.js
@@ -1,14 +1,48 @@
 import React, {Component} from 'react';
+import constants from '../constants';
+import classNames from 'classnames';
+import './MyFriend.css';
 
 class MyFriends extends Component {
+  state = {
+    metrics: constants.myFriendMetrics.peopleIFollow
+  };
+
+  renderNavigator() {
+    return (
+      <div className='my-friend-navigator'>
+        {Object.keys(constants.myFriendMetrics).map(key => {
+          return (
+            <div key={key}
+                 className={classNames("my-friends-selector", key)}
+                 onClick={this.showMetrics.bind(this, key)}>
+              {constants.myFriendMetrics[key]}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  renderUserList() {
+
+  }
+
+  showMetrics(key) {
+    this.setState({
+      metrics: constants.myFriendMetrics[key],
+    })
+  }
 
   render() {
     return (
-      <div>
-        MyFriends
+      <div className='my-friend'>
+        {this.renderNavigator()}
+        {this.renderUserList()}
       </div>
     );
   }
+
 }
 
 export default MyFriends;
---
+++ src/components/constants.js
@@ -0,0 +1,9 @@
+const constants = {
+ myFriendMetrics: {
+   peopleIFollow: 'People I Follow',
+   peopleFollowingMe: 'People Following Me',
+   myFriends: 'My Friends',
+ }
+}
+
+export default constants;
